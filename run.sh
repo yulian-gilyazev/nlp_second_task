5f7aa365fdfa34a4740ed4f70dd5e05f541d8065
j.gilyazev <j.gilyazev@vk.team>
2023-04-21 15:40:30 +0300

    added training

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
-python3 src/inference.py --train-data data/train.csv --test-data data/test.csv --config configs/config.json --model-path models/ditilroberta-base-finetuned
+
+python3 src/train.py --train-data data/train.csv --config configs/config.json --model-path models/distilroberta-base-finetuned
+python3 src/inference.py --train-data data/train.csv --test-data data/test.csv --prediction-path data/submission.txt --config configs/config.json --model-path models/distilroberta-base-finetuned/checkpoint-112
